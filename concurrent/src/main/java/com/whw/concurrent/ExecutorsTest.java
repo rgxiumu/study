d85e40147a2ccaee0e7c4dfd1c5f8e8f986b27f4
whw <710405940@qq.com>
2020-08-10 20:34:18 +0800

    自定义线程池

---
+++ concurrent/src/main/java/com/whw/concurrent/ExecutorsTest.java
@@ -0,0 +1,59 @@
+package com.whw.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * TODO
+ *
+ * @author wuhongwei
+ * @version 1.0
+ * @date 2020/8/10
+ */
+public class ExecutorsTest {
+
+    private static AtomicInteger aa = new AtomicInteger();
+
+    public static void main(String[] args) {
+        System.out.println("111111");
+        new Thread(()->{
+            System.out.println("1112222");
+        }).start();
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,
+                3,
+                2,
+                TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(2), new ThreadFactory() {
+            private AtomicInteger atomicInteger = new AtomicInteger();
+            @Override
+            public Thread newThread(Runnable r) {
+                System.out.println("create thread");
+                return new Thread(r,"tee"+atomicInteger.incrementAndGet());
+            }
+        });
+
+        threadPoolExecutor.execute(()->{
+            aa.incrementAndGet();
+            System.out.println("test11111---"+Thread.currentThread());
+        });
+        threadPoolExecutor.execute(()->{
+            aa.incrementAndGet();
+            System.out.println("test22222---"+Thread.currentThread());
+        });
+        threadPoolExecutor.execute(()->{
+            aa.incrementAndGet();
+            System.out.println("test33333---"+Thread.currentThread());
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(aa.get());
+        Executors.newCachedThreadPool();
+
+    }
+}
